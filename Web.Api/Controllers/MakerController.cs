1a0f19859737b750fad9cc517392c0ab618ab328
Nik Steel <nikolassteel@gmail.com>
2020-02-19 18:02:32 -0500

    init .net core app

---
+++ Web.Api/Controllers/MakerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MakerController : ControllerBase
+    {
+
+        private readonly ILogger<MakerController> _logger;
+
+        public MakerController(ILogger<MakerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world";
+        }
+    }
+}
